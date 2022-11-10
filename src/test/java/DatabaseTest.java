0ee8932ffe2bb9fe11190abcaf6d3737a6f7cbd6
amirah2700 <amir2700@hotmail.com>
2022-11-10 13:56:05 +0100

    database test

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -6,21 +6,49 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatabaseTest {
 
 
+    // ADD SUPERHERO
     @Test
     void addSuperhero() {
         //Opret Superhelt objekt og tilføj til arraylist
         //Arrange
         //Initialisere 'database'
         Database database = new Database();
-        // int expectedSize = 1; **
 
         //Act
         database.addSuperhero("Clark Kent", "Super Man", "Everything", 1938, 9.9);
-        // int actualSize = database.getAllSuperheroes().size(); **
+        int actualSize = database.getHeros().size();
 
         //Assert
-        assertEquals(1,database.getHeros().size());
+        assertEquals(4, actualSize);
         // ** En anden måde at skrive det overstående linje.
 
+    }
+
+    // UPDATE SUPERHERO
+    @Test
+    void updateSuperhero() {
+        Database database = new Database();
+
+        //Act
+        database.addSuperhero("Clark Kent", "Super Man", "Everything", 1938, 9.9);
+        int actualSize = database.getHeros().size();
+        database.getHeros().get(actualSize - 1).setStrength(20);
+
+        //Assert
+        assertEquals(20, 20);
+    }
+
+    // DELETE SUPERHERO
+    @Test
+    void deleteSuperhero() {
+        Database database = new Database();
+
+        //Act
+        database.addSuperhero("Clark Kent", "Super Man", "Everything", 1938, 9.9);
+        int actualSize = database.getHeros().size();
+        database.removeSuperhero(actualSize - 1);
+
+        //Assert
+        assertEquals(4, actualSize);
     }
 }
